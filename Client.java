bdb7f83311f1edac55349c3f2ec55bf0209e0a55
SteelPotathor <riouslain@gmail.com>
2023-11-20 11:26:09 +0100

    MultiChat Console

--- Client.java
+++ Client.java
@@ -1,2 +1,71 @@
-public class Client {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client implements Runnable {
+
+    private Socket client;
+    private BufferedReader in;
+    private PrintWriter out;
+    private boolean done;
+
+    public void shutdown() {
+        done = true;
+        try {
+            in.close();
+            out.close();
+            if (!client.isClosed()) {
+                client.close();
+            }
+        } catch (IOException e) {
+            System.out.println("Error shutting down client");
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            client = new Socket("localhost", 9999);
+            in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            out = new PrintWriter(client.getOutputStream(), true);
+            InputHandler inputHandler = new InputHandler();
+            Thread thread = new Thread(inputHandler);
+            thread.start();
+            String inMessage;
+            while ((inMessage = in.readLine()) != null) {
+                System.out.println(inMessage);
+            }
+        } catch (IOException e) {
+            shutdown();
+        }
+    }
+
+    class InputHandler implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                BufferedReader inReader = new BufferedReader(new InputStreamReader(System.in));
+                while (!done) {
+                    String message = inReader.readLine();
+                    if (message.equals("/bye")) {
+                        out.println(message);
+                        inReader.close();
+                        shutdown();
+                    } else {
+                        out.println(message);
+                    }
+                }
+            } catch (IOException e) {
+                shutdown();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
 }
--- Server.java
+++ Server.java
@@ -1,2 +1,125 @@
-public class Server {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Server implements Runnable {
+
+    private ArrayList<ConnectionHandler> connections;
+    private ServerSocket server;
+    private boolean done;
+    private ExecutorService pool;
+    public Server() {
+        connections = new ArrayList<ConnectionHandler>();
+        done = false;
+    }
+
+    public void broadcast(String message) {
+        for (ConnectionHandler connection : connections) {
+            if (connection != null) {
+                connection.sendMessage(message);
+            }
+        }
+    }
+
+    public void shutdown() {
+        try {
+            done = true;
+            pool.shutdown();
+            if (!server.isClosed()) {
+                server.close();
+            }
+            for (ConnectionHandler connection : connections) {
+                if (connection != null) {
+                    connection.sendMessage("Server shutting down");
+                    connection.shutdown();
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error shutting down server");
+        }
+    }
+    @Override
+    public void run() {
+        try {
+            server = new ServerSocket(9999);
+            pool = Executors.newCachedThreadPool();
+            while (!done) {
+                Socket client = server.accept();
+                ConnectionHandler handler = new ConnectionHandler(client);
+                connections.add(handler);
+                pool.execute(handler);
+            }
+        } catch (Exception e) {
+            shutdown();
+        }
+    }
+
+    class ConnectionHandler implements Runnable {
+
+        private Socket client;
+        private BufferedReader in;
+        private PrintWriter out;
+        private String nickname;
+
+        public ConnectionHandler(Socket client) {
+            this.client = client;
+        }
+
+        public void sendMessage(String message) {
+            out.println(message);
+        }
+
+        public void shutdown() {
+            try {
+                in.close();
+                out.close();
+                if (!client.isClosed()) {
+                    client.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Error closing connection");
+            }
+        }
+        @Override
+        public void run() {
+            try {
+                out = new PrintWriter(client.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                out.println("Please enter a nickname: ");
+                nickname = in.readLine();
+                System.out.println(nickname + " connected");
+                broadcast(nickname + " joined the chat!");
+                String message;
+                while ((message = in.readLine()) != null) {
+                    if (message.startsWith("/nick ")) {
+                        String[] messageSplit = message.split(" ", 2);
+                        if (messageSplit.length == 2) {
+                            broadcast(nickname + " changed their nickname to " + messageSplit[1]);
+                            System.out.println(nickname + " changed their nickname to " + messageSplit[1]);
+                            nickname = messageSplit[1];
+                            out.println("Nickname changed to " + nickname);
+                        } else {
+                            out.println("Invalid nickname");
+                        }
+                    } else if (message.startsWith("/bye")) {
+                        broadcast(nickname + " left the chat");
+                        shutdown();
+                    } else
+                        broadcast(nickname + ": " + message);
+                }
+            } catch(IOException e) {
+                shutdown();
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run();
+    }
 }
